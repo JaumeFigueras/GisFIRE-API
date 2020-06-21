eed288012b416524b717d887207cce49c8a3aac0
JaumeFigueras <jaume.figueras@upc.edu>
2020-06-21 19:45:22 +0200

    Bonjour request

--- tokens/tokens.py
+++ tokens/tokens.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_httpauth import HTTPBasicAuth
+from flask import request
 
 import random
 import psycopg2
@@ -10,12 +11,15 @@ auth = HTTPBasicAuth()
 
 CONFIG = configparser.ConfigParser()
 CONFIG.read('/home/gisfire/gisfire.cfg')
-DB_CONNECTION = psycopg2.connect(host=CONFIG['database']['host'],
-                                    port=CONFIG['database']['port'],
-                                    database=CONFIG['database']['database'],
-                                    user=CONFIG['database']['user'],
-                                    password=CONFIG['database']['password'])
 CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ:_!$€@"
+DB_CONNECTION = None
+
+def restore_db_connection()
+    DB_CONNECTION = psycopg2.connect(host=CONFIG['database']['host'],
+                                        port=CONFIG['database']['port'],
+                                        database=CONFIG['database']['database'],
+                                        user=CONFIG['database']['user'],
+                                        password=CONFIG['database']['password'])
 
 def get_random_string(length):
     token = ""
@@ -29,12 +33,26 @@ def verify_password(username, password):
 
 @app.route('/', methods=['GET'])
 def hello_world():
+    inet = request.remote_addr
+    sql = "INSERT INTO access (token_id, ip) VALUES (NULL, '{0:}')".format(inet)
+    if DB_CONNECTION is None:
+        restore_db_connection()
+    cursor = DB_CONNECTION.cursor()
+    cursor.execute(sql)
+    DB_CONNECTION.commit()
+    cursor.close()
     return 'GisFIRE API: tokens'
 
 @app.route('/token', methods=['POST'])
 @auth.login_required
 def token():
     return 'GisFIRE API:'
+
+@app.teardown_appcontext
+def close_db(error):
+    if DB_CONNECTION is not None:
+        DB_CONNECTION.close()
+
 
 if __name__ == "__main__":
     app.run()
