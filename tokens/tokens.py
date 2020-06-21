323695b8b2973e9df88109cf3706292a524eee44
JaumeFigueras <jaume.figueras@upc.edu>
2020-06-21 20:02:20 +0200

    minor error

--- tokens/tokens.py
+++ tokens/tokens.py
@@ -15,11 +15,12 @@ CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ:_!$€@"
 DB_CONNECTION = None
 
 def restore_db_connection():
-    global DB_CONNECTION = psycopg2.connect(host=CONFIG['database']['host'],
-                                            port=CONFIG['database']['port'],
-                                            database=CONFIG['database']['database'],
-                                            user=CONFIG['database']['user'],
-                                            password=CONFIG['database']['password'])
+    global DB_CONNECTION
+    DB_CONNECTION = psycopg2.connect(host=CONFIG['database']['host'],
+                                        port=CONFIG['database']['port'],
+                                        database=CONFIG['database']['database'],
+                                        user=CONFIG['database']['user'],
+                                        password=CONFIG['database']['password'])
 
 def get_random_string(length):
     token = ""
