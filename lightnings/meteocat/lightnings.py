727a1bc2baff5476b35c125f4eb9af19a5c5389d
JaumeFigueras <jaume.figueras@upc.edu>
2020-07-01 16:43:26 +0200

    added download lightnings from meteocat function

--- lightnings/meteocat/lightnings.py
+++ lightnings/meteocat/lightnings.py
@@ -9,9 +9,36 @@ import psycopg2
 import configparser
 import datetime
 import dateutil.parser
+import concurrent.futures
+import requests
 
 app = Flask(__name__)
 auth = HTTPBasicAuth()
+
+def download_thread(year, month, day, hour, api_key):
+    """Download function of the meteo.cat dataof the selected date and hour. It
+    uses tha API KEY provided bu the user to access the meteo.cat resources
+
+    :param date: the date of the data to download
+    :type date: datetime.date
+
+    :param hour: the hour of the data to download
+    :type hour: int
+
+    :param api_key: the API KEY provided by meteo.cat to access its resources
+    :type api_key: string
+
+    :return: the success of the download operation and data received as json
+    object extracted from the server response
+    :type return: (bool, object)
+    """
+    base_url = "https://api.meteo.cat/xdde/v1"
+    query = "/catalunya/{0:04d}/{1:02d}/{2:02d}/{3:02d}".format(year, month, day, hour)
+    url = base_url + query
+    headers = {"x-api-key": "{0:}".format(api_key)}
+    r = requests.get(url, headers=headers)
+    return (r.status_code, r.json())
+
 
 @app.before_request
 def config_setup():
@@ -34,7 +61,7 @@ def close_db(error):
     g.DB_GISFIRE.close()
 
 @app.errorhandler(401)
-def page_not_found(error):
+def unauthorized(error):
     return jsonify({'status_code': 401}), 401
 
 @app.errorhandler(404)
@@ -88,57 +115,135 @@ def hello_world():
 
 @app.route('/<year>/<month>/<day>/<hour>', methods=['GET'])
 @auth.login_required()
-def retrieve_lightnings():
-    # If there aren't all the required paramaters a bad request is thrown
-    if request.method != 'DELETE':
-        if 'username' not in request.json or 'valid_until' not in request.json:
-            return jsonify({'status_code': 400, 'message': 'username and valid_until parameters are expected'}), 400
-    else:
-        if 'username' not in request.json:
-            return jsonify({'status_code': 400, 'message': 'username parameter is expected'}), 400
-    # Get data
+def retrieve_lightnings(year, month, day, hour):
+    # Get basic data
     user = auth.current_user()
     inet = request.remote_addr
-    username = request.json['username']
-    # If valid_until format is not valid a bad request is thrown
-    if request.method != 'DELETE':
-        try:
-            valid_until = dateutil.parser.parse(request.json['valid_until'])
-        except:
-            return jsonify({'status_code': 400, 'message': 'invalid valid_until date format'}), 400
-        # If valid_until is not grater than today a bad request is thrown
-        if datetime.datetime.utcnow() > valid_until:
-            return jsonify({'status_code': 400, 'message': 'invalid valid_until date value'}), 400
-        password = get_random_string(64)
-    # Build queries
-    sql_access = "INSERT INTO access (token_id, ip, url, method) VALUES ({0:}, '{1:}', '/token', '{2:}')".format(user['id'], inet, request.method)
-    if request.method == 'POST':
-        sql_tokens = "INSERT INTO tokens (username, token, admin, valid_until) VALUES (%s, '{0}', FALSE, '{1}')".format(password, valid_until.strftime("%Y-%m-%dT%H:%M:%SZ"))
-    elif request.method == 'PUT':
-        sql_tokens = "UPDATE tokens SET token = '{0}', valid_until = '{1}' WHERE username = %s".format(password, valid_until.strftime("%Y-%m-%dT%H:%M:%SZ"))
-    else:
-        sql_tokens = "DELETE FROM tokens WHERE username = %s"
-    # Execute commands
+    # Try to convert all parameters to integers
+    try:
+        year = int(year)
+        month = int(month)
+        day = int(day)
+        hour = int(hour)
+    except:
+        return jsonify({'status_code': 400, 'message': 'invalid paramaters'}), 400
+    # If all the parameters are correct, is a possible date?
+    if year < 1900 or year > 9999:
+        return jsonify({'status_code': 400, 'message': 'invalid year'}), 400
+    if month < 0 or month > 12:
+        return jsonify({'status_code': 400, 'message': 'invalid month'}), 400
+    if day < 0 or day > 31:
+        return jsonify({'status_code': 400, 'message': 'invalid day'}), 400
+    if hour < 0 or hour > 23:
+        return jsonify({'status_code': 400, 'message': 'invalid hour'}), 400
+    # It seems a date but is the 31 of february?
+    try:
+        date = datetime.datetime(year=year, month=month, day=day, hour=hour)
+        if date > datetime.datetime.utcnow():
+            return jsonify({'status_code': 400, 'message': 'invalid date'}), 400
+    except:
+        return jsonify({'status_code': 400, 'message': 'invalid date'}), 400
+    # The date is valid, let's log the access
+    sql_access = "INSERT INTO access (token_id, ip, url, method) VALUES ({0:}, '{1:}', '/lightnings/meteocat/v1/{3:}/{4:}/{5:}/{6:}', '{2:}')".format(user['id'], inet, request.method, year, month, day, hour)
+    try:
+        cursor = g.DB_GISFIRE.cursor()
+        cursor.execute(sql_access)
+        if cursor.rowcount != 1:
+            cursor.close()
+            g.DB_GISFIRE.rollback()
+            return jsonify({'status_code': 500, 'message': 'failed log action'}), 500
+        g.DB_GISFIRE.commit()
+        cursor.close()
+    except:
+        return jsonify({'status_code': 500, 'message': 'failed log action'}), 500
+    # Now check if this request is neo or not
+    sql_lightnings = "SELECT result_code, number_of_lightnings FROM xdde_requests \
+                        WHERE year = {0:} AND month = {1:} AND day = {2:} AND hour = {3:}"
+                        .format(year, month, day, hour)
     try:
         cursor = g.DB_CONNECTION.cursor()
-        cursor.execute(sql_tokens, (username, ))
-        if cursor.rowcount != 1:
-            cursor.close()
-            g.DB_CONNECTION.rollback()
-            return jsonify({'status_code': 500, 'message': 'failed token action'}), 500
-        cursor.execute(sql_access, (username, ))
-        if cursor.rowcount != 1:
-            cursor.close()
-            g.DB_CONNECTION.rollback()
-            return jsonify({'status_code': 500, 'message': 'failed log action'}), 500
-        g.DB_CONNECTION.commit()
-        cursor.close()
-        if request.method != 'DELETE':
-            return jsonify({'username': username, 'token': password})
+        cursor.execute(sql_lightnings)
+        if cursor.rowcount > 1:
+            return jsonify({'status_code': 500, 'message': 'database inconsistency on xdde_requests'}), 500
+        elif cursor.rowcount == 1:
+            rows = cursor.fetchall()
+            new_request = False
+            old_request_status_code = rows[0][0]
         else:
-            return jsonify({'username': username})
+            new_request = True
+        cursor.close()
     except:
-        return jsonify({'status_code': 500, 'message': 'sql exception'}), 500
+        return jsonify({'status_code': 500, 'message': 'failed request check'}), 500
+    # Launch request to MeteoCat
+    if new_request or (not new_request and old_request_status_code != 200):
+        meteocat_api_key = request.headers.get('x-api-key')
+        return_code, lightnings = download_thread(year, month, day, hour, meteocat_api_key)
+        if return_code == 200:
+            if new_request:
+                sql_lightnings = "INSERT INTO xdde_requests (year, month, day, hour, result_code, number_of_lightnings)\
+                                    VALUES ({0:}, {1:}, {2:}, {3:}, {4:}, {5:}, )".format(year, month, day, hour, return_code, len(lightnings))
+            else:
+                sql_lightnings = "UPDATE xdde_requests SET result_code = {0:}, number_of_lightnings = {1:}".format(return_code, len(lightnings))
+        else:
+            sql_lightnings = "INSERT INTO xdde_requests (year, month, day, hour, result_code)\
+                                VALUES ({0:}, {1:}, {2:}, {3:}, {4:})".format(year, month, day, hour, return_code)
+        try:
+            cursor = g.DB_CONNECTION.cursor()
+            cursor.execute(sql_lightnings)
+            if cursor.rowcount =! 1:
+                g.DB_CONNECTION.rollback()
+                cursor.close()
+                return jsonify({'status_code': 500, 'message': 'database error on xdde_requests'}), 500
+            g.DB_CONNECTION.commit()
+            cursor.close()
+            if return_code != 200:
+                return jsonify({'status_code': 502, 'message': 'error while accessing remote server'}), 502
+        except:
+            return jsonify({'status_code': 500, 'message': 'database error on xdde_requests'}), 500
+        # At this point the database has to be populated with lightnings
+        sql_lightnings = list()
+        for ln in lightnings:
+            sql_lightnings.append("INSERT INTO lightnings (_id, _data, _correntPic, _chi2, _ellipse_eixMajor, _ellipse_eixMenor, _ellipse_angle, _numSensors, _nuvolTerra, _idMunicipi, _coordenades_latitud, _coordenades_longitud) \
+                                    VALUES ({0:}, {1:}, {2:}, {3:}, {4:}, {5:}, {6:}, {7:}, {8:}, {9:}, {10:}, )").format(
+                                    ln['id'], ln['data'], ln['correntPic'], ln['chi2'], ln['ellipse']['eixMajor'], ln['ellipse']['eixMenor'], ln['ellipse']['angle'], ln['numSensors'], ln['nuvolTerra'], ln['idMunicipi'] if 'idMunicipi' in ln else 'NULL', ln['coordenades']['longitud'], ln['coordenades']['latitud'])
+                                    )
+        try:
+            cursor = g.DB_CONNECTION.cursor()
+            cursor.execute(sql_lightnings)
+            if cursor.rowcount =! len(sql_lightnings):
+                g.DB_CONNECTION.rollback()
+                cursor.close()
+                return jsonify({'status_code': 500, 'message': 'database error on lightnings'}), 500
+            g.DB_CONNECTION.commit()
+            cursor.close()
+        except:
+            return jsonify({'status_code': 500, 'message': 'database error on lightnings'}), 500
+    # Read the database and prepare json
+    sql_lightnings = "SELECT _id, _data, _correntPic, _chi2, _ellipse_eixMajor, _ellipse_eixMenor, _ellipse_angle, _numSensors, _nuvolTerra, _idMunicipi, _coordenades_latitud, _coordenades_longitud FROM lightnings WHERE _data >= '{0:}:{1:}:{2:} {3:}-00-00'::timestamp AND _data <= '{0:}:{1:}:{2:} {3:}-59-59'::timestamp"
+    cursor = g.DB_CONNECTION.cursor()
+    cursor.execute(sql_lightnings)
+    rows = cursor.fetchall()
+    lightnings = list()
+    for row in rows:
+        ln = dict()
+        ln['id'] = row[0]
+        ln['data'] = row[1]
+        ln['correntPic'] = row[2]
+        ln['chi2'] = row[3]
+        ln['ellipse'] = dict()
+        ln['ellipse']['eixMajor'] = row[4]
+        ln['ellipse']['eixMenor'] = row[5]
+        ln['ellipse']['angle'] = row[6]
+        ln['numSnesors'] = row[7]
+        ln['nuvolTerra'] = row[8]
+        ln['idMunicipi'] = row[9]
+        ln['coordenades'] = dict()
+        ln['coordenades']['longitud'] = row[10]
+        ln['coordenades']['latitud'] = row[11]
+        lightnings.append(ln)
+    # Return data
+    return jsonify(lighnings)
+
 
 if __name__ == "__main__":
     app.run()
