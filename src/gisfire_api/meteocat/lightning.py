6d8f87add90087a2a0db0501e0145e711513a4ba
JaumeFigueras <jaume.figueras@upc.edu>
2022-03-23 20:18:56 +0100

    another try

--- src/gisfire_api/meteocat/lightning.py
+++ src/gisfire_api/meteocat/lightning.py
@@ -134,10 +134,10 @@ def get_lightnings(year, month, day):
         app.json_encoder = Lightning.GeoJSONEncoder
     else:
         app.json_encoder = Lightning.JSONEncoder
-    return jsonify(lightnings[0].x), 200
+    txt = jsonify(lightnings[0].x)
     UserAccess(request.remote_addr, request.url, request.method, json.dumps(dict(request.values)),
                auth.current_user()).record_access(db)
-    return jsonify(lightnings), 200
+    return txt, 200
 
 
 def get_from_remote(current_date: datetime.datetime, lightnings: List[Lightning]) -> Tuple[List[Lightning], int]:
